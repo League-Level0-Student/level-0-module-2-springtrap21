2953e2aa27e9480553846a83dbfe52df397505be
s.weise <s.weise@sw-pc>
2019-01-15 18:17:15 -0800

    rpsrandomstuff

--- src/random/IntroToRandom.java
+++ src/random/IntroToRandom.java
@@ -13,17 +13,22 @@ public class IntroToRandom {
 		//3. Now make r random. 
 		//   Create an object of the Random class
 		//	 use .nextInt() to get a random number
-		
-
+		Random rnd =new Random();
+		r = rnd.nextInt();
 		//4. Limit the random number between 0 and 100
-
+		r = rnd.nextInt(101);
 		//5. Limit the random number between 25 and 75. Hint: ((highest value - lowest value)+1) + lowest value
-
+		r= rnd.nextInt(51)+25;
 		//6. Challenge: Limit the random number between -222 and 88
-
-		//1. Print out the value of r
+		int count = 10;
+		for (int i = 0; i < count; i++) {
+			
 		
+		r = rnd.nextInt(311)-222;
+		//1. Print out the value of r
+		System.out.println(r);
+		}
 		//2. Run the program.  What number appears in the console?
 		//   Run it again. Is the number the same?
-	}
+	}         //yeah
 }
--- src/random/RockPaperScissors.java
+++ src/random/RockPaperScissors.java
@@ -6,6 +6,7 @@ package random;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -75,15 +76,14 @@ public class RockPaperScissors extends JPanel implements ActionListener{
     public void actionPerformed(ActionEvent e) {
         
         //1. Run the program and play many rounds of Rock Paper Scissors. Does the computer always choose the same thing?
-
+    	//yes
         //2. Change the value of opponentSelection to be a random number between 0 and 2;
 	//   Don't forget to create a Random object.
-	    
-        //2. Change the value of opponentSelection to be a random number between 0 and 2; 
-        int opponentSelection = 0;
+	    Random rnd = new Random();
+        int opponentSelection = rnd.nextInt(3);
         
         //3. Run the program again. Is the result different?
- 
+        //yes the choice of the computer is random
         int selection = 0;
        
         if(e.getSource() == paperButton){
