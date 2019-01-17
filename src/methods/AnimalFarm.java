0829242afd8f086713d5156cd949617699d58fc6
ilabstaff <ilabstaff@172.16.116.81>
2019-01-16 19:31:07 -0800

    dicerollandanimalfarm

--- src/methods/AnimalFarm.java
+++ src/methods/AnimalFarm.java
@@ -15,11 +15,41 @@ public class AnimalFarm {
 	AnimalFarm() {
 		/*
 		 * 1. Ask the user which animal they want, then play the sound of that animal.
-		 */
+		 */ 
+		String grinch = JOptionPane.showInputDialog(null,"What farm animal do you want? tell me, or the Grinch will get you++" );
 		
 		/* 2. Make it so that the user can keep entering new animals. */
+		if (grinch.equals("cow")) {
+			playMoo();
+		}
+		if (grinch.equals("duck")) {
+			playQuack();
+		}	
+		if (grinch.equals("dog")) {
+			playWoof();
+		}
+		if (grinch.equals("cat")) {
+			playMeow();
+		}
+		if (grinch.equals("llama")) {
+			playLlama();
+		}
+		
+		
+		
+		
+		
+	
 	}
-
+	void playMeow() {
+		playNoise(meowFile);
+	}
+	void playLlama() {
+		playNoise(llamaFile);
+	}
+	
+	
+	
 	void playMoo() {
 		playNoise(mooFile);
 	}
